e496d161c49446d3f432439223877184e42161ec
alessandro1606 <alessandro160604@gmail.com>
2025-12-04 23:30:17 -0300

    Add files via upload

---
+++ main/java/repository/AlunoRepository.java
@@ -0,0 +1,8 @@
+package com.aaw.avaliacao.repository;
+
+import com.aaw.avaliacao.model.Aluno;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AlunoRepository extends JpaRepository<Aluno, Long> { }
+
+
---
+++ main/java/repository/CursoRepository.java
@@ -0,0 +1,7 @@
+package com.aaw.avaliacao.repository;
+
+import com.aaw.avaliacao.model.Curso;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CursoRepository extends JpaRepository<Curso, Long> { }
+
